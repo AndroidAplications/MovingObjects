977128422012dd2c2434ca67ad7d65ab0876ce5c
laizpablo <93laizpablo@gmail.com>
2017-02-28 20:29:23 +0100

    print coord

--- app/src/main/java/androidaplications/pablolaiz/movingobjects/TouchActivity.java
+++ app/src/main/java/androidaplications/pablolaiz/movingobjects/TouchActivity.java
@@ -3,6 +3,7 @@ package androidaplications.pablolaiz.movingobjects;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -38,24 +39,31 @@ public class TouchActivity extends Activity {
 
                 final int x = (int) event.getRawX();
                 final int y = (int) event.getRawY();
-
+                Log.i("onTouchListener", "Coordinates: ")
+                Log.i("onTouchListener", "x: " + x);
+                Log.i("onTouchListener", "y: " + y);
                 switch (event.getAction() & MotionEvent.ACTION_MASK) {
 
                     case MotionEvent.ACTION_DOWN:
+                        //Log.i("ACTION_DOWN", "In ACTION_DOWN");
                         RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
                                 view.getLayoutParams();
 
                         xDelta = x - lParams.leftMargin;
                         yDelta = y - lParams.topMargin;
+                        //Log.i("ACTION_DOWN", "xDelta: " + xDelta);
+                        //Log.i("ACTION_DOWN", "yDelta: " + yDelta);
                         break;
 
                     case MotionEvent.ACTION_UP:
+                        //Log.i("ACTION_UP", "In ACTION_UP");
                         Toast.makeText(TouchActivity.this,
                                 "Everybody love Pablo!", Toast.LENGTH_SHORT)
                                 .show();
                         break;
 
                     case MotionEvent.ACTION_MOVE:
+                        //Log.i("ACTION_MOVE", "In ACTION_MOVE");
                         RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
                                 .getLayoutParams();
                         layoutParams.leftMargin = x - xDelta;
